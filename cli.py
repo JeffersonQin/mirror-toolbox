016f16106026dde07a1d4d2911f6cadcec10cce1
gyro永不抽风 <1247006353@qq.com>
2022-03-16 19:21:23 +0800

    refactor: oop

--- cli.py
+++ cli.py
@@ -1,121 +1,5 @@
 import click
-import cv2
-import win32api
-import win32con
-import win32ui
-import win32gui
-import time
-import traceback
-import numpy as np
-
-
-window_class = 'Qt5151QWindowIcon'
-window_name_prefix = 'Classroom_'
-window_exe = 'ClassIn.exe'
-
-# TODO: 以后写 win32api 自动检测
-dpi_scale = 1
-bmpfilenamename = "screenshot.bmp"
-
-g_hwnd = None
-g_fps = 60.0
-
-def _cerr(caller: str, message: str):
-	click.echo(click.style(f"[{caller}]", bg='magenta', fg='white'), nl=False)
-	click.echo(click.style(f" {message}", fg = 'bright_red'))
-
-
-def cerr(caller, *messages):
-	val = ''
-	for message in messages:
-		val = val + ' ' + str(message)
-	_cerr(caller, val)
-
-
-def enum_callback(hwnd, params):
-	if not str(win32gui.GetWindowText(hwnd)).startswith(window_name_prefix): return
-	if win32gui.GetClassName(hwnd) != window_class: return
-	global g_hwnd
-	g_hwnd = hwnd	
-
-
-def capture_screen(hwnd, x1, y1, x2, y2):
-	# show window
-	win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
-	# get window position
-	rect = win32gui.GetWindowRect(hwnd)
-	cx = rect[0]
-	cy = rect[1]
-	cw = int((rect[2] - cx) * dpi_scale)
-	ch = int((rect[3] - cy) * dpi_scale)
-	# get screenshot param
-	x = cx + x1 * dpi_scale
-	y = cy + y1 * dpi_scale
-	w = min((x2 - x1) * dpi_scale, cw)
-	h = min((y2 - y1) * dpi_scale, ch)
-	# take screenshot
-	wDC = win32gui.GetWindowDC(hwnd)
-	dcObj=win32ui.CreateDCFromHandle(wDC)
-	cDC=dcObj.CreateCompatibleDC()
-	dataBitMap = win32ui.CreateBitmap()
-	dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-	cDC.SelectObject(dataBitMap)
-	cDC.BitBlt((0, 0), (w, h) , dcObj, (x1, y1), win32con.SRCCOPY)
-	dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
-	# Free Resources
-	dcObj.DeleteDC()
-	cDC.DeleteDC()
-	win32gui.ReleaseDC(hwnd, wDC)
-	win32gui.DeleteObject(dataBitMap.GetHandle())
-
-
-def update_rotate(side, pos, delta):
-	pos += delta
-	if pos > 1:
-		pos -= 1
-		side = (side + 1) % 4
-	return side, pos
-
-
-def calc_rotate(side, pos, width, height):
-	if side == 0:
-		return width * pos, 0
-	if side == 1:
-		return width, height * pos
-	if side == 2:
-		return width * (1 - pos), height
-	if side == 3:
-		return 0, height * (1 - pos)
-
-
-class StatusSwitcher:
-
-	def __init__(self, status, duration, interval):
-		'''Constructor of StatusSwitcher
-		===============================
-		* :param: status: initial status
-		* :param: duration: duration of status (True)
-		* :param: interval: interval (False) between status
-		'''
-		self.status = status
-		self.duration = duration
-		self.interval = interval
-		self.counter = 0.0
-		self.last_time = time.time()
-
-	def update(self):
-		now_time = time.time()
-		self.counter += now_time - self.last_time
-		self.last_time = now_time
-
-		if self.status is True:
-			if self.counter >= self.duration:
-				self.status = False
-				self.counter %= self.duration
-		else:
-			if self.counter >= self.interval:
-				self.status = True
-				self.counter %= self.interval
+from toolbox import ToolBox
 
 
 @click.group()
@@ -136,6 +20,7 @@ def cli():
 @click.option('--affine-rotate-speed', default=10, type=float, help='affine rotate speed (% / second)')
 # inverse effect
 @click.option('--use-inverse-effect', default=False, type=bool, help='use inverse effect')
+@click.option('--inverse-effect-duration', default=0.05, type=float, help='inverse effect duration (second)')
 @click.option('--inverse-effect-interval', default=0.05, type=float, help='inverse effect interval (second)')
 # rgb split glitch effect
 @click.option('--use-rgb-split-glitch', default=False, type=bool, help='use rgb split glitch art effect')
@@ -158,7 +43,7 @@ def start(
 	hwnd, 
 	x1, y1, x2, y2, 
 	use_affine_rotate, affine_rotate_speed, 
-	use_inverse_effect, inverse_effect_interval, 
+	use_inverse_effect, inverse_effect_duration, inverse_effect_interval, 
 	use_rgb_split_glitch, use_tiktok_style, 
 	glitch_duration, glitch_interval, 
 	glitch_rx_offset_mean, glitch_gx_offset_mean, glitch_bx_offset_mean,
@@ -168,98 +53,39 @@ def start(
 	'''
 	Start toolbox
 	'''
-	global g_fps
+	
+	toolbox_instance = ToolBox(hwnd)
+	toolbox_instance.x1 = x1
+	toolbox_instance.y1 = y1
+	toolbox_instance.x2 = x2
+	toolbox_instance.y2 = y2
+	toolbox_instance.use_affine_rotate = use_affine_rotate
+	toolbox_instance.affine_rotate_speed = affine_rotate_speed
+	toolbox_instance.use_inverse_effect = use_inverse_effect
+	toolbox_instance.inverse_switcher.duration = inverse_effect_duration
+	toolbox_instance.inverse_switcher.interval = inverse_effect_interval
+	toolbox_instance.use_rgb_split_glitch = use_rgb_split_glitch
+	toolbox_instance.glitch_switcher.duration = glitch_duration
+	toolbox_instance.glitch_switcher.interval = glitch_interval
+	toolbox_instance.glitch_rx_offset_mean = glitch_rx_offset_mean
+	toolbox_instance.glitch_gx_offset_mean = glitch_gx_offset_mean
+	toolbox_instance.glitch_bx_offset_mean = glitch_bx_offset_mean
+	toolbox_instance.glitch_ry_offset_mean = glitch_ry_offset_mean
+	toolbox_instance.glitch_gy_offset_mean = glitch_gy_offset_mean
+	toolbox_instance.glitch_by_offset_mean = glitch_by_offset_mean
+	toolbox_instance.glitch_rx_offset_sd = glitch_rx_offset_sd
+	toolbox_instance.glitch_gx_offset_sd = glitch_gx_offset_sd
+	toolbox_instance.glitch_bx_offset_sd = glitch_bx_offset_sd
+	toolbox_instance.glitch_ry_offset_sd = glitch_ry_offset_sd
+	toolbox_instance.glitch_gy_offset_sd = glitch_gy_offset_sd
+	toolbox_instance.glitch_by_offset_sd = glitch_by_offset_sd
 
-	# hwnd
-	if hwnd is None:
-		win32gui.EnumWindows(enum_callback, None)
-		if g_hwnd is None:
-			raise Exception('No window found')
-		hwnd = g_hwnd
-	# shape
-	width = x2 - x1
-	height = y2 - y1
-
-	# rotate top left
-	rtl_side, rtl_pos = 0, 0.0
-	# rotate top right
-	rtr_side, rtr_pos = 1, 0.0
-
-	# tiktok
 	if use_tiktok_style:
-		glitch_duration = 1
-		glitch_interval = 0.00001
-		glitch_rx_offset_mean = 0.0
-		glitch_gx_offset_mean = 0.0
-		glitch_bx_offset_mean = 0.0
-		glitch_ry_offset_mean = 0.0
-		glitch_gy_offset_mean = 0.0
-		glitch_by_offset_mean = 0.0
-
-	# inverse
-	inverse_switcher = StatusSwitcher(False, inverse_effect_interval, inverse_effect_interval)
-
-	# glitch
-	glitch_switcher = StatusSwitcher(False, glitch_duration, glitch_interval)
+		toolbox_instance.use_tiktok()
 
 	while True:
-		# used to calculate fps
-		start_time = time.time()
-
-		try:
-			capture_screen(hwnd, x1, y1, x2, y2)
-		except Exception as e:
-			cerr('capture_screen', repr(e))
-			cerr('capture_screen', traceback.format_exc())
-			continue
-
-		img = cv2.imread(bmpfilenamename)
-		img_processed = img
-
-		if use_affine_rotate:
-			rtl_side, rtl_pos = update_rotate(rtl_side, rtl_pos, affine_rotate_speed / g_fps)
-			rtr_side, rtr_pos = update_rotate(rtr_side, rtr_pos, affine_rotate_speed / g_fps)
-
-			rtl_x, rtl_y = calc_rotate(rtl_side, rtl_pos, width, height)
-			rtr_x, rtr_y = calc_rotate(rtr_side, rtr_pos, width, height)
-
-			affine_matrix = cv2.getAffineTransform(
-				np.float32([[width / 2.0, height / 2.0], [0, 0], [width, 0]]),
-				np.float32([[width / 2.0, height / 2.0], [rtl_x, rtl_y], [rtr_x, rtr_y]])
-			)
-			
-			img_processed = cv2.warpAffine(img_processed, affine_matrix, (width, height))
-
-		if use_inverse_effect:
-			inverse_switcher.update()
-			if inverse_switcher.status:
-				img_processed = cv2.bitwise_not(img_processed)
-
-		if use_rgb_split_glitch:
-			glitch_switcher.update()
-			if glitch_switcher.status:
-				uxr = np.random.normal(glitch_rx_offset_mean, glitch_rx_offset_sd)
-				uxg = np.random.normal(glitch_gx_offset_mean, glitch_gx_offset_sd)
-				uxb = np.random.normal(glitch_bx_offset_mean, glitch_bx_offset_sd)
-				uyr = np.random.normal(glitch_ry_offset_mean, glitch_ry_offset_sd)
-				uyg = np.random.normal(glitch_gy_offset_mean, glitch_gy_offset_sd)
-				uyb = np.random.normal(glitch_by_offset_mean, glitch_by_offset_sd)
-
-				img_processed[:, :, 0] = cv2.warpAffine(img_processed[:, :, 0], 
-					np.float32([[1, 0, uxr], [0, 1, uyr]]), (width, height))
-				img_processed[:, :, 1] = cv2.warpAffine(img_processed[:, :, 1], 
-					np.float32([[1, 0, uxg], [0, 1, uyg]]), (width, height))
-				img_processed[:, :, 2] = cv2.warpAffine(img_processed[:, :, 2], 
-					np.float32([[1, 0, uxb], [0, 1, uyb]]), (width, height))
-
-		cv2.imshow('screenshot', img_processed)
-		cv2.waitKey(1)
-
-		# calculate fps
-		end_time = time.time()
-		g_fps = 1.0 / (end_time - start_time)
-
-		print('fps: {:.2f}'.format(g_fps))
+		toolbox_instance.show(toolbox_instance.process_fps)
+		print('fps: {:.2f}'.format(toolbox_instance.process_fps))
 
 
 if __name__ == '__main__':
---
+++ toolbox.py
@@ -0,0 +1,241 @@
+import click
+import cv2
+import win32api
+import win32con
+import win32ui
+import win32gui
+import time
+import traceback
+import numpy as np
+
+window_class = 'Qt5151QWindowIcon'
+window_name_prefix = 'Classroom_'
+window_exe = 'ClassIn.exe'
+
+# TODO: 以后写 win32api 自动检测
+dpi_scale = 1
+bmpfilenamename = "screenshot.bmp"
+
+g_hwnd = None
+
+
+def _cerr(caller: str, message: str):
+	click.echo(click.style(f"[{caller}]", bg='magenta', fg='white'), nl=False)
+	click.echo(click.style(f" {message}", fg = 'bright_red'))
+
+
+def cerr(caller, *messages):
+	val = ''
+	for message in messages:
+		val = val + ' ' + str(message)
+	_cerr(caller, val)
+
+
+def enum_callback(hwnd, _):
+	if not str(win32gui.GetWindowText(hwnd)).startswith(window_name_prefix): return
+	if win32gui.GetClassName(hwnd) != window_class: return
+	global g_hwnd
+	g_hwnd = hwnd	
+
+
+def update_rotate(side, pos, delta):
+	pos += delta
+	if pos > 1:
+		pos -= 1
+		side = (side + 1) % 4
+	return side, pos
+
+
+def calc_rotate(side, pos, width, height):
+	if side == 0:
+		return width * pos, 0
+	if side == 1:
+		return width, height * pos
+	if side == 2:
+		return width * (1 - pos), height
+	if side == 3:
+		return 0, height * (1 - pos)
+
+
+class StatusSwitcher:
+
+	def __init__(self, status, duration, interval):
+		'''Constructor of StatusSwitcher
+		===============================
+		* :param: status: initial status
+		* :param: duration: duration of status (True)
+		* :param: interval: interval (False) between status
+		'''
+		self.status = status
+		self.duration = duration
+		self.interval = interval
+		self.counter = 0.0
+		self.last_time = time.time()
+
+
+	def update(self):
+		now_time = time.time()
+		self.counter += now_time - self.last_time
+		self.last_time = now_time
+
+		if self.status is True:
+			comparer = self.interval if self.duration is None else self.duration
+			if self.counter >= comparer:
+				self.status = False
+				self.counter %= self.duration
+		else:
+			comparer = self.duration if self.interval is None else self.interval
+			if self.counter >= comparer:
+				self.status = True
+				self.counter %= self.interval
+
+
+class ToolBox:
+
+	hwnd: int = None
+
+	x1: int = 0
+	y1: int = 0
+	x2: int = 0
+	y2: int = 0
+	
+	use_affine_rotate: bool = False
+	affine_rotate_speed: float = 10.0
+	
+	use_inverse_effect: bool = False
+	inverse_switcher = StatusSwitcher(False, 0.05, 0.05)
+	
+	use_rgb_split_glitch: bool = False
+	glitch_switcher = StatusSwitcher(False, 0.5, 0.5)
+	glitch_rx_offset_mean: float = 5.0
+	glitch_gx_offset_mean: float = -5.0
+	glitch_bx_offset_mean: float = 3.0
+	glitch_ry_offset_mean: float = 5.0
+	glitch_gy_offset_mean: float = -5.0
+	glitch_by_offset_mean: float = 3.0
+	glitch_rx_offset_sd: float = 2.0
+	glitch_gx_offset_sd: float = 2.0
+	glitch_bx_offset_sd: float = 2.0
+	glitch_ry_offset_sd: float = 2.0
+	glitch_gy_offset_sd: float = 2.0
+	glitch_by_offset_sd: float = 2.0
+
+	process_fps = 0.00000001
+
+
+	def capture_screen(self):
+		# show window
+		win32gui.ShowWindow(self.hwnd, win32con.SW_RESTORE)
+		# get window position
+		rect = win32gui.GetWindowRect(self.hwnd)
+		cx = rect[0]
+		cy = rect[1]
+		cw = int((rect[2] - cx) * dpi_scale)
+		ch = int((rect[3] - cy) * dpi_scale)
+		# get screenshot param
+		x = cx + self.x1 * dpi_scale
+		y = cy + self.y1 * dpi_scale
+		w = min((self.x2 - self.x1) * dpi_scale, cw)
+		h = min((self.y2 - self.y1) * dpi_scale, ch)
+		# take screenshot
+		wDC = win32gui.GetWindowDC(self.hwnd)
+		dcObj=win32ui.CreateDCFromHandle(wDC)
+		cDC=dcObj.CreateCompatibleDC()
+		dataBitMap = win32ui.CreateBitmap()
+		dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+		cDC.SelectObject(dataBitMap)
+		cDC.BitBlt((0, 0), (w, h) , dcObj, (self.x1, self.y1), win32con.SRCCOPY)
+		dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+		# Free Resources
+		dcObj.DeleteDC()
+		cDC.DeleteDC()
+		win32gui.ReleaseDC(self.hwnd, wDC)
+		win32gui.DeleteObject(dataBitMap.GetHandle())
+
+
+	def __init__(self, hwnd):
+		if hwnd is None:
+			win32gui.EnumWindows(enum_callback, None)
+			if g_hwnd is None:
+				raise Exception('No window found')
+			hwnd = g_hwnd
+		self.hwnd = hwnd
+
+		# rotate top left
+		self.rtl_side, self.rtl_pos = 0, 0.0
+		# rotate top right
+		self.rtr_side, self.rtr_pos = 1, 0.0
+
+
+	def use_tiktok(self):
+		self.glitch_switcher.glitch_duration = 1
+		self.glitch_switcher.glitch_interval = 0.00001
+		self.glitch_rx_offset_mean = 0.0
+		self.glitch_gx_offset_mean = 0.0
+		self.glitch_bx_offset_mean = 0.0
+		self.glitch_ry_offset_mean = 0.0
+		self.glitch_gy_offset_mean = 0.0
+		self.glitch_by_offset_mean = 0.0
+
+
+	def show(self, fps):
+		# used to calculate fps
+		process_start = time.time()
+
+		# shape
+		width = self.x2 - self.x1
+		height = self.y2 - self.y1
+
+		# capture screen
+		try:
+			self.capture_screen()
+		except Exception as e:
+			cerr('capture_screen', repr(e))
+			cerr('capture_screen', traceback.format_exc())
+			return
+
+		img = cv2.imread(bmpfilenamename)
+		img_processed = img
+
+		if self.use_affine_rotate:
+			self.rtl_side, self.rtl_pos = update_rotate(self.rtl_side, self.rtl_pos, self.affine_rotate_speed / fps)
+			self.rtr_side, self.rtr_pos = update_rotate(self.rtr_side, self.rtr_pos, self.affine_rotate_speed / fps)
+
+			rtl_x, rtl_y = calc_rotate(self.rtl_side, self.rtl_pos, width, height)
+			rtr_x, rtr_y = calc_rotate(self.rtr_side, self.rtr_pos, width, height)
+
+			affine_matrix = cv2.getAffineTransform(
+				np.float32([[width / 2.0, height / 2.0], [0, 0], [width, 0]]),
+				np.float32([[width / 2.0, height / 2.0], [rtl_x, rtl_y], [rtr_x, rtr_y]])
+			)
+			
+			img_processed = cv2.warpAffine(img_processed, affine_matrix, (width, height))
+
+		if self.use_inverse_effect:
+			self.inverse_switcher.update()
+			if self.inverse_switcher.status:
+				img_processed = cv2.bitwise_not(img_processed)
+
+		if self.use_rgb_split_glitch:
+			self.glitch_switcher.update()
+			if self.glitch_switcher.status:
+				uxr = np.random.normal(self.glitch_rx_offset_mean, self.glitch_rx_offset_sd)
+				uxg = np.random.normal(self.glitch_gx_offset_mean, self.glitch_gx_offset_sd)
+				uxb = np.random.normal(self.glitch_bx_offset_mean, self.glitch_bx_offset_sd)
+				uyr = np.random.normal(self.glitch_ry_offset_mean, self.glitch_ry_offset_sd)
+				uyg = np.random.normal(self.glitch_gy_offset_mean, self.glitch_gy_offset_sd)
+				uyb = np.random.normal(self.glitch_by_offset_mean, self.glitch_by_offset_sd)
+
+				img_processed[:, :, 0] = cv2.warpAffine(img_processed[:, :, 0], 
+					np.float32([[1, 0, uxr], [0, 1, uyr]]), (width, height))
+				img_processed[:, :, 1] = cv2.warpAffine(img_processed[:, :, 1], 
+					np.float32([[1, 0, uxg], [0, 1, uyg]]), (width, height))
+				img_processed[:, :, 2] = cv2.warpAffine(img_processed[:, :, 2], 
+					np.float32([[1, 0, uxb], [0, 1, uyb]]), (width, height))
+
+		cv2.imshow('screenshot', img_processed)
+		cv2.waitKey(1)
+
+		# calculate fps
+		process_end = time.time()
+		self.process_fps = 1.0 / (process_end - process_start)
